bdc44535ff4fb4bcaebbd1a2d3abcd90af8c6f03
Mehdi2211 <mahdimk.kar2005@gmail.com>
2026-03-31 10:14:33 +0100

    Integration gestion soumission + publications

---
+++ Desktop/Smart_Reaseach_Publication_Management-main/mainwindow.h
@@ -0,0 +1,55 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { 
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+class PublicationsPage;
+class Submission;
+class Conference;
+class Laboratoire;
+class Evaluation;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void initUserPage();
+    void onAccueilClicked();
+    void onPublicationsClicked();
+    void onUtilisateursClicked();
+    void onSoumissionsClicked();
+    void onEvaluationsClicked();
+    void onConferencesClicked();
+    void onLaboratoiresClicked();
+
+private:
+    Ui::MainWindow *ui;
+    PublicationsPage *publicationsPage;
+    Submission *submissionPage;
+    Conference *conferencePage;
+    Laboratoire *laboratoirePage;
+    Evaluation *evaluationPage;
+    
+    void loadStyleSheet();
+    void setupConnections();
+    void setupIcons();
+    void setupPublicationsPage();
+    void setupSubmissionPage();
+    void setupEvaluationPage();
+    void setupConferencePage();
+    void setupLaboratoirePage();
+    void updateTitleUnderline(const QString &color);
+};
+
+#endif // MAINWINDOW_H
